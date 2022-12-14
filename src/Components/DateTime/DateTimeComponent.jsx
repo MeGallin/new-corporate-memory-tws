403bbd5915623d870c6a7f85e55c2bc13a23a36d
MeGallin <me@garyallin.uk>
2022-12-14 14:47:08 +0000

    Footer working

---
+++ src/Components/DateTime/DateTimeComponent.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from 'react';
+import moment from 'moment';
+
+const DateTime = () => {
+  const [dateTime, setDateTime] = useState('');
+
+  useEffect(() => {
+    setInterval(() => {
+      setDateTime(moment().format('MMMM Do YYYY, h:mm:ss a'));
+    }, 1000);
+  }, [dateTime]);
+
+  return <>{dateTime ? <div>{dateTime}</div> : null}</>;
+};
+
+export default DateTime;

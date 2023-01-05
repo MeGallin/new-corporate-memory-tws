795706a4c513b66c535d7cda6552346071ef7e0b
MeGallin <me@garyallin.uk>
2023-01-05 20:48:27 +0000

    about content added

--- src/Components/About/AboutComponent.jsx
+++ src/Components/About/AboutComponent.jsx
@@ -5,17 +5,28 @@ const AboutComponent = () => {
     <fieldset className="fieldSet">
       <legend>About Us</legend>
       <p>
-        YCM, is a simple to use productivity tool that can be used to save,
-        retrieve and edit information in a secure environment.
+        YCM is a productivity tool that is easy to use and allows you to save,
+        retrieve, and edit information in a secure environment.
       </p>
       <p>
-        Simply, create an account confirm your email address and log into your
-        secure dashboard.
+        To get started, create an account, confirm your email address, and log
+        into your secure dashboard. From there, you can create new memories, add
+        optional tags, and set reminders.
       </p>
-      <p>Create a new memory, add a an optional tag and set a reminder.</p>
-      <p>There is no limit to the number of memories.</p>
+      <p>There is no limit to the number of memories that you can create.</p>
+
       <h1>The Tech</h1>
-      <p>Build using the MERN stack </p>
+      <p>Construct using the MERN stack </p>
+      <p>
+        The MERN stack is a popular stack of technologies for building web
+        applications. It consists of four open-source components: MongoDB, a
+        NoSQL database; Express, a web application framework for Node.js; React,
+        a JavaScript library for building user interfaces; and Node.js, a
+        runtime environment for executing JavaScript code on the server side.
+        Together, these technologies provide a strong foundation for building
+        web applications with a robust and scalable back-end, a powerful and
+        flexible front-end, and a common language for both.
+      </p>
       <h1>Contact</h1>
       <p>Feel free to contact us via our contact form.</p>
     </fieldset>
